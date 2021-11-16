66b20389e97cd5690a9310232b3061446d550fa9
Sarmad Junaid <sarmadjkhan@gmail.com>
2021-11-16 11:29:47 +0500

    sorted data by published date

--- api/parsers.py
+++ api/parsers.py
@@ -3,11 +3,12 @@ import feedparser
 
 def get_feed_data(urls):
     items = []
+    sorted_data = []
     for url in urls:
         data = feedparser.parse(f'{url}')
         items.append(data.entries)
-    
-    
-
-    return items
+    for i in range(len(items)):
+        sorted_data.append(sorted(items[i], key=lambda item: item.published)[:20])
+    print(sorted_data)
+    return sorted_data
 
